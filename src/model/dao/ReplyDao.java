98a07db0b93a186e4910344cc6289f1fa1c90376
challenger1736 <challenger1736@gmail.com>
2024-02-01 16:19:58 +0900

    신승호/0201replycontent ReplyDao에서 찾을수 없음

--- src/model/dao/ReplyDao.java
+++ src/model/dao/ReplyDao.java
@@ -1,7 +1,9 @@
 package model.dao;
 
 import model.dto.EmployeeDTO;
+import model.dto.PartDTO;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,25 +18,35 @@ public class ReplyDao extends SuperDao{
     public ArrayList<Map> replyview(int boardno){ // 리플들을 담을 배열 빼오기.
         ArrayList<Map> result = new ArrayList<>();
         try{
-            String sql = "select * from reply where boardno = ? order by replyno;";
+            String sql = "select*from reply where boardno = ? order by replyno;";
             ps = conn.prepareStatement(sql);
             ps.setInt(1, boardno);
             rs = ps.executeQuery();
             while (rs.next()){// 한 줄(레코드) 단위로 소환.
                 Map<String, String> map1 = new HashMap<>();
                 // 리플 하나 하나당 있을 데이터 , 작성자, 부서, 쓴 날짜, 내용
-                int eno = rs.getInt("eno"); // 작성자 정보 받을 로직짜기.
-                EmployeeDTO employeeDTO = enoSearch(eno);
-                map1.put("ename", employeeDTO.getEname());
-                map1.put("partname", partnoSearch(employeeDTO.getPartno()).getPartname());
-                System.out.println(rs.getString("replycontent"));
+//                int eno = rs.getInt("eno"); // 작성자 정보 받을 로직짜기.
+//                EmployeeDTO employeeDTO = enoSearch(eno);
+//                map1.put("ename", employeeDTO.getEname());
+//                map1.put("partname", partnoSearch(employeeDTO.getPartno()).getPartname());
+                map1.put("eno", rs.getString("eno"));
+                map1.put("replyno", rs.getString("replyno"));
                 map1.put("replycontent" , rs.getString("replycontent"));
                 map1.put("replydate", rs.getString("replydate"));
                 result.add(map1); // 댓글 하나 어레이리스트에 넣기
             }
+            for(int i=0 ; i<result.size(); i++){
+                EmployeeDTO employeeDTO = enoSearch(Integer.parseInt((String)(result.get(i).get("eno"))));
+                // 어레이리스트 i번쨰 인덱스 map 키값 eno의 String을 정수로 변환해서 받아온 회원 정보들을 employeeDTO에 저장.
+                result.get(i).put("ename",employeeDTO.getEname()); // 그 놈은 그 i번쨰 어레이리스트(맵) 하나에 ename으로 저장.
+                result.get(i).put("partname",partnoSearch(employeeDTO.getPartno()).getPartname()); // 이 놈은 그 i번쨰 어레이리스트(맵) 하나에 partname으로 저장.
+            }
+
+
         }catch (Exception e){
             System.out.println(e);
         }
         return result;
-    }
+    } // 리뷰 보이기 로직
+
 }
