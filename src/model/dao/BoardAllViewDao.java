be5e2ceb54f65f205375d38902695370a36bed5f
y2k8861 <y2k8861@naver.com>
2024-02-01 18:26:13 +0900

    20240201  이승호 전체글출력 완료 및 글쓰기 시작 커밋

--- src/model/dao/BoardAllViewDao.java
+++ src/model/dao/BoardAllViewDao.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import controller.BoadAllViewController;
 import model.dto.BoardDTO;
+import model.dto.EmployeeDTO;
 
 import java.util.ArrayList;
 
@@ -10,34 +11,37 @@ public class BoardAllViewDao extends SuperDao {
     private static BoardAllViewDao boardAllViewDao = new BoardAllViewDao();
     public static BoardAllViewDao getInstance(){return boardAllViewDao;}
 
-    public ArrayList<BoardDTO> boardAllView(int ch){
+    public ArrayList<BoardDTO> boardAllView(int ch, EmployeeDTO loginstate){
         ArrayList<BoardDTO> boardDTOArrayList = new ArrayList<>();
         // [5] 받은 ch마다 카테고리 값 변경
         String sql = null;
-        if(ch == 1){    // [5-1] 전체글보기 = board게시판 모든글 가져오기
-            try {
+        try {
+            if(ch == 1){    // [5-1] 전체글보기 = board게시판 모든글 가져오기
                 sql = "select * from board order by boardno;";
                 ps = conn.prepareStatement(sql);
-                rs = ps.executeQuery();
-                while (rs.next()){  //  rs.next() -> 데이터베이스 테이블에서 한칸이동후 값이 있으면 true 없으면 false
-                    // true 일때
-                    BoardDTO boardDto = new BoardDTO();
-                    boardDto.setEno(rs.getInt("eno"));  // cno 가져와 객체에 넣어준다.
-                    boardDto.setBoardno(rs.getInt("boardno"));  // mno 가져와 객체에 넣어준다.
-                    boardDto.setBoardtitle(rs.getString("boardtitle"));  // bno 가져와 객체에 넣어준다.
-                    boardDto.setBoardcontent(rs.getString("boardcontent")); // btitle 가져와 객체에 넣어준다.
-                    boardDto.setBoarddate(rs.getString("boarddate"));   // bdate 가져와 객체에 넣어준다.
-                    boardDTOArrayList.add(boardDto); // 9.필요한 자료를 넣은 객체를 배열에 넣어준다.
-                }   // false(다음께 없으면) 종료
-                return boardDTOArrayList;
-            } catch (Exception e){
-                System.out.println(e);
+            } else if (ch == 2){    // [5-2] 직급게시판 = 현재로그인된 직급
+                sql = "SELECT * FROM board JOIN boardpermit ON board.boardno = boardpermit.boardno where boardpermit.gradeno = ?;";
+                ps = conn.prepareStatement(sql);
+                ps.setInt(1, loginstate.getGradeno());
+            } else if (ch == 3){    // [5-3] 부서별게시판 = 현재 로그인된 부서
+                sql = "SELECT * FROM board JOIN boardpermit ON board.boardno = boardpermit.boardno where boardpermit.partno = ?;";
+                ps = conn.prepareStatement(sql);
+                ps.setInt(1, loginstate.getPartno());
             }
-
-        } else if (ch == 2){    // [5-2] 직급게시판 = 현재로그인된 직급
-            sql = "select * from board where gradno = ? order by boardno;";
-        } else if (ch == 3){    // [5-3] 부서별게시판 = 현재 로그인된 부서
-            sql = "select * from board order by boardno;";
+            rs = ps.executeQuery();
+            while (rs.next()){  //  rs.next() -> 데이터베이스 테이블에서 한칸이동후 값이 있으면 true 없으면 false
+                // true 일때
+                BoardDTO boardDto = new BoardDTO();
+                boardDto.setEno(rs.getInt("eno"));  // eno 가져와 객체에 넣어준다.
+                boardDto.setBoardno(rs.getInt("boardno"));  // boardno 가져와 객체에 넣어준다.
+                boardDto.setBoardtitle(rs.getString("boardtitle"));  // boardtitle 가져와 객체에 넣어준다.
+                boardDto.setBoardcontent(rs.getString("boardcontent")); // boardcontent 가져와 객체에 넣어준다.
+                boardDto.setBoarddate(rs.getString("boarddate"));   // boarddate 가져와 객체에 넣어준다.
+                boardDTOArrayList.add(boardDto); // 9.필요한 자료를 넣은 객체를 배열에 넣어준다.
+            }   // false(다음께 없으면) 종료
+            return boardDTOArrayList;
+        } catch (Exception e){
+            System.out.println(e);
         }
 
         /*//2.sql 기재
